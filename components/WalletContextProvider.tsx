b55adc5c56d01d3e20bd1b3d2c19bd4c763a9768
paritomarrr <pari2019cse083@gmail.com>
2022-12-12 23:24:48 +0530

    wallet udated

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,21 +1,23 @@
-import { FC, ReactNode } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+import { FC, ReactNode } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import * as web3 from "@solana/web3.js"
-import * as walletAdapterWllets from '@solana/wallet-adapter-wallets'
-require('@solana/wallet-adapter-react-ui/styles.css')
+import * as web3 from '@solana/web3.js'
+import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <ConnectionProvider endpoint={""}>
-      <WalletProvider wallets={[]}>
-        <WalletModalProvider>{children}</WalletModalProvider>
+  const endpoint = web3.clusterApiUrl('devnet')
+	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
+
+	return (
+		<ConnectionProvider endpoint={endpoint}>
+	    <WalletProvider wallets={wallets}>
+	      <WalletModalProvider>
+	        { children }
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
-  );
-};
+	)
+}
 
-export default WalletContextProvider;
+export default WalletContextProvider
